bfabd546b55539d4c7bc2cfd0f23430f6ea3a4d3
sibil <volvoriney@gmail.com>
2021-01-25 01:44:47 +0300

    guessing number game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello Dima");
+use std::io;
+
+fn main(){
+    println!("This is guessing game");
+    println!("Just input some number please!");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("Your guessed number is: {}", guess);
 }
